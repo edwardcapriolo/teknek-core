ec9bfb4f8b9aa7608602a068c5b32e0897bf7c2c
Edward Capriolo <edward.capriolo@huffingtonpost.com>
2014-05-16 16:21:06 -0400

    Better startup script and a log4j properties file

--- src/main/scripts/start-teknek.sh
+++ src/main/scripts/start-teknek.sh
@@ -2,5 +2,4 @@ CP=`ls teknek-core*.jar`
 for f in `ls lib/*` ; do
   CP=$CP:$f
 done
-java -Dteknek.zk.servers=localhost:2181 -cp $CP io.teknek.daemon.TeknekDaemon
-
+java -Dlog4j.configuration=log4j.properties -Dteknek.zk.servers=localhost:2181 -cp $CP:. io.teknek.daemon.TeknekDaemon
