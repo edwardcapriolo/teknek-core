e171a7f68340b1b934c49ecc6b6fe12fa01efbfb
Edward Capriolo <edlinuxguru@gmail.com>
2015-12-06 20:39:54 -0500

    Stuff

--- src/test/java/io/teknek/zookeeper/TestRestablishingKeeper.java
+++ src/test/java/io/teknek/zookeeper/TestRestablishingKeeper.java
@@ -8,21 +8,21 @@ import io.teknek.plan.Plan;
 import junit.framework.Assert;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.test.InstanceSpec;
 import org.apache.curator.test.QuorumConfigBuilder;
 import org.apache.curator.test.TestingZooKeeperServer;
 import org.apache.zookeeper.ZooKeeper;
 import org.junit.Test;
 
-public class TestRestablishingKeeper {
+public class TestRestablishingKeeper extends EmbeddedZooKeeperServer {
   
   @Test
   public void showAutoRecovery() throws Exception {
-    TestingZooKeeperServer zookeeperTestServer = new TestingZooKeeperServer(new QuorumConfigBuilder());
-    zookeeperTestServer.start();
-    int port = zookeeperTestServer.getInstanceSpec().getPort();
     Properties p = new Properties();
     p.put(TeknekDaemon.ZK_SERVER_LIST, zookeeperTestServer.getInstanceSpec().getConnectString());
     final TeknekDaemon td = new TeknekDaemon(p);
+    int port = zookeeperTestServer.getInstanceSpec().getPort();
+    System.out.println(zookeeperTestServer.getInstanceSpec().getConnectString());
     RestablishingKeeper k = new RestablishingKeeper(zookeeperTestServer.getInstanceSpec().getConnectString()){
       @Override
       public void onReconnect(ZooKeeper zk, CuratorFramework framework){
@@ -37,21 +37,19 @@ public class TestRestablishingKeeper {
     Plan plan = new Plan().withName("abc");
     td.getWorkerDao().createOrUpdatePlan(plan, k.getZooKeeper());
     Assert.assertEquals(1, td.getWorkerDao().finalAllPlanNames(k.getZooKeeper()).size());
-    zookeeperTestServer.close();
+    zookeeperTestServer.stop();
     try {
       td.getWorkerDao().finalAllPlanNames(k.getZooKeeper());
       Assert.fail("Dao should have failed");
     } catch (Exception ex){ }
-    TestingZooKeeperServer revive = new TestingZooKeeperServer(new QuorumConfigBuilder(), port);
     try {
       Assert.assertEquals(1, td.getWorkerDao().finalAllPlanNames(k.getZooKeeper()).size());
       Assert.fail("Dao should have failed");
-    } catch (Exception ex){
-      
-    }
+    } catch (Exception ex){ }
+    zookeeperTestServer.start();
     Thread.sleep(3000);
     Assert.assertEquals(0, td.getWorkerDao().finalAllPlanNames(k.getZooKeeper()).size());
     Assert.assertEquals(2, k.getReestablished());
-    revive.close();
+    
   }
 }
